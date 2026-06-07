47974d0298d4b875d8bca64d6af840f22538e582
SoulDev07 <mayankravariya@gmail.com>
2026-06-07 21:35:27 +0530

    add email service and configuration for sending emails

---
+++ src/main/java/net/souldev07/journalApp/service/EmailService.java
@@ -0,0 +1,29 @@
+package net.souldev07.journalApp.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendEmail(String to, String subject, String body) {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo(to);
+            message.setSubject(subject);
+            message.setText(body);
+            mailSender.send(message);
+        } catch (MailException e) {
+            log.error("Error occurred while sending email", e);
+        }
+    }
+}
