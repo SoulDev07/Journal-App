ec71d58f9e3c06f690760c5e639f46df68617379
SoulDev07 <mayankravariya@gmail.com>
2026-06-16 17:13:05 +0530

    add swagger

---
+++ src/main/java/net/souldev07/journalApp/config/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package net.souldev07.journalApp.config;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI myCustomConfig() {
+        return new OpenAPI()
+                .info(new Info().title("Journal App APIs"))
+                .servers(Arrays.asList(new Server().url("http://localhost:8080").description("local")))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components().addSecuritySchemes("bearerAuth",
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
